a4c9df38d8c4f3956e38797deff51117c888400e
Lau Skeeter <lawr@lauragonzalez.cc>
2018-10-01 21:11:09 +0100

    fix all calcs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const t2n = require('text-to-number');
 
 module.exports = {
-	add: (...nums) => nums.reduce((acc, cur) => acc + t2n(cur), 0),
-	substract: (...nums) => nums.reduce((acc, cur) => acc - t2n(cur), 0),
+	add: (...nums) => 
+		[...nums].splice(1).reduce((acc, cur) => acc + t2n(cur), t2n(nums[0])),
+	substract: (...nums) => 
+		[...nums].splice(1).reduce((acc, cur) => acc - t2n(cur), t2n(nums[0])),
 	multiply: (...nums) =>
 		[...nums].splice(1).reduce((acc, cur) => acc * t2n(cur), t2n(nums[0])),
 	divide: (...nums) =>

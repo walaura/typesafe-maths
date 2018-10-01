b011a7ac0276035ab8532dfa82101946d9393ee5
Lau Skeeter <lawr@lauragonzalez.cc>
2018-10-01 12:32:43 +0100

    initial

---
+++ index.test.js
@@ -0,0 +1,31 @@
+const { add, substract, multiply, divide } = require('./index.js');
+
+test('1 + 2 to equal 3', () => {
+	expect(add('one', 'two')).toBe(3);
+	expect(add('one', '2')).toBe(3);
+	expect(add(1, '2')).toBe(3);
+});
+
+test('1 - 2 to equal -3', () => {
+	expect(substract('one', 'two')).toBe(-3);
+	expect(substract('one', '2')).toBe(-3);
+	expect(substract(1, '2')).toBe(-3);
+});
+
+test('10 / 5 to equal 2', () => {
+	expect(divide('ten', 'five')).toBe(2);
+	expect(divide('ten', '5')).toBe(2);
+	expect(divide(10, '5')).toBe(2);
+	expect(divide(10, '5')).toBe(2);
+});
+
+test('10 * 5 to equal 50', () => {
+	expect(multiply('ten', 'five')).toBe(50);
+	expect(multiply('ten', '5')).toBe(50);
+	expect(multiply(10, '5')).toBe(50);
+});
+
+test('Operate on N numbers', () => {
+	expect(multiply('ten', 'five', 'two', 'four')).toBe(400);
+	expect(divide('ten', 'five', 'two', 'four')).toBe(0.25);
+});

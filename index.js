da87104855425e3f5709d1788118d257c923e35b
Lau Skeeter <lawr@lauragonzalez.cc>
2018-10-01 21:17:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const t2n = require('text-to-number');
 module.exports = {
 	add: (...nums) => nums.map(t2n).reduce((acc, cur) => acc + cur),
 	substract: (...nums) => nums.map(t2n).reduce((acc, cur) => acc - cur),
-	multiply: (...nums) => nums.map(t2n).reduce((acc, cur) * acc * cur),
+	multiply: (...nums) => nums.map(t2n).reduce((acc, cur) => acc * cur),
 	divide: (...nums) => nums.map(t2n).reduce((acc, cur) => acc / cur),
 };

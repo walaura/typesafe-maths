2f2ba9a89856c4c45de87bee12cb3afa57e8708a
Lau Skeeter <lawr@lauragonzalez.cc>
2018-10-01 21:12:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const t2n = require('text-to-number');
 
 module.exports = {
-	add: (...nums) => 
-		[...nums].splice(1).reduce((acc, cur) => acc + t2n(cur), t2n(nums[0])),
-	substract: (...nums) => 
-		[...nums].splice(1).reduce((acc, cur) => acc - t2n(cur), t2n(nums[0])),
-	multiply: (...nums) =>
-		[...nums].splice(1).reduce((acc, cur) => acc * t2n(cur), t2n(nums[0])),
-	divide: (...nums) =>
-		[...nums].splice(1).reduce((acc, cur) => acc / t2n(cur), t2n(nums[0])),
+	add: (...nums) => nums.reduce((acc, cur) => acc + t2n(cur)),
+	substract: (...nums) => nums.reduce((acc, cur) => acc - t2n(cur)),
+	multiply: (...nums) => nums.reduce((acc, cur) => acc * t2n(cur)),
+	divide: (...nums) => nums.reduce((acc, cur) => acc / t2n(cur)),
 };

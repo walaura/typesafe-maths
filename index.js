8e722eab366a2c99a328e13b6f1f8fef8daf5326
RichieAHB <richard.beddington@guardian.co.uk>
2018-10-03 17:12:50 +0100

    Monadify

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const t2n = require('text-to-number');
 
+const toI = v => (typeof v === 'string' ? t2n(v) : v);
+
+const create = fn =>
+  function aux(total) {
+    const totalI = toI(total);
+    return Object.defineProperty(n => aux(fn(totalI, toI(n))), 'value', {
+      get: () => totalI
+    });
+  };
+
 module.exports = {
-	add: (...nums) => nums.map(t2n).reduce((acc, cur) => acc + cur),
-	substract: (...nums) => nums.map(t2n).reduce((acc, cur) => acc - cur),
-	multiply: (...nums) => nums.map(t2n).reduce((acc, cur) => acc * cur),
-	divide: (...nums) => nums.map(t2n).reduce((acc, cur) => acc / cur),
+  add: create((a, b) => a + b),
+  subtract: create((a, b) => a - b),
+  multiply: create((a, b) => a * b),
+  divide: create((a, b) => a / b)
 };
--- index.test.js
+++ index.test.js
@@ -1,31 +1,36 @@
-const { add, substract, multiply, divide } = require('./index.js');
+const { add, subtract, multiply, divide } = require('./index.js');
 
 test('1 + 2 to equal 3', () => {
-	expect(add('one', 'two')).toBe(3);
-	expect(add('one', '2')).toBe(3);
-	expect(add(1, '2')).toBe(3);
+  expect(add('one')('two').value).toBe(3);
+  expect(add('one')('2').value).toBe(3);
+  expect(add(1)('2').value).toBe(3);
 });
 
 test('1 - 4 to equal -3', () => {
-	expect(substract('one', 'four')).toBe(-3);
-	expect(substract('one', '4')).toBe(-3);
-	expect(substract(1, '4')).toBe(-3);
+  expect(subtract('one')('four').value).toBe(-3);
+  expect(subtract('one')('4').value).toBe(-3);
+  expect(subtract(1)('4').value).toBe(-3);
 });
 
 test('10 / 5 to equal 2', () => {
-	expect(divide('ten', 'five')).toBe(2);
-	expect(divide('ten', '5')).toBe(2);
-	expect(divide(10, '5')).toBe(2);
-	expect(divide(10, '5')).toBe(2);
+  expect(divide('ten')('five').value).toBe(2);
+  expect(divide('ten')('5').value).toBe(2);
+  expect(divide(10)('5').value).toBe(2);
+  expect(divide(10)('5').value).toBe(2);
 });
 
 test('10 * 5 to equal 50', () => {
-	expect(multiply('ten', 'five')).toBe(50);
-	expect(multiply('ten', '5')).toBe(50);
-	expect(multiply(10, '5')).toBe(50);
+  expect(multiply('ten')('five').value).toBe(50);
+  expect(multiply('ten')('5').value).toBe(50);
+  expect(multiply(10)('5').value).toBe(50);
 });
 
 test('Operate on N numbers', () => {
-	expect(multiply('ten', 'five', 'two', 'four')).toBe(400);
-	expect(divide('ten', 'five', 'two', 'four')).toBe(0.25);
+  expect(multiply('ten')('five')('two')('four').value).toBe(400);
+  expect(divide('ten')('five')('two')('four').value).toBe(0.25);
+});
+
+test('Allows negative numbers in the after the first arg', () => {
+  expect(multiply('ten')('five')(-2)('four').value).toBe(-400);
+  expect(divide('ten')('five')(-2)('four').value).toBe(-0.25);
 });
